b8133943fdab27f389483b6516f947e6dd877795
Jau004 <125649061+Jau004@users.noreply.github.com>
2024-10-21 14:02:14 +0200

    lagt til gjennomsnitt

--- Python_Files/SemesterOppgave.py
+++ Python_Files/SemesterOppgave.py
@@ -38,8 +38,15 @@ nord_nox_year = genereate_random_year_dataList(intencity=.3, seed = 1)
 #lille_nox_year = genereate_random_year_dataList(intencity=.6, seed = 3)
 kron_asfaltstov_year = genereate_random_year_dataList(intencity=2.0, seed = 5)
 nord_asfaltstov_year = genereate_random_year_dataList(intencity=0.7, seed = 4)
-lille_asfaltstov_year = genereate_random_year_dataList(intencity=1.2, seed = 2)
+#lille_asfaltstov_year = genereate_random_year_dataList(intencity=1.2, seed = 2)
 
+def gjennomsnitt(nordnes, kronstad):
+    avg = []
+    for index in range(0,len(nordnes)):
+        avg.append((nordnes[index]+kronstad[index])/2)
+    return avg
+
+gjen_nox_year = gjennomsnitt(kron_nox_year,nord_nox_year)
 
 #create figure and 3 axis
 fig = plt.figure(figsize=(13, 5))
@@ -53,7 +60,7 @@ axInterval.patch.set_alpha(0.5)
 coordinates_Nordnes = (90, 60)
 coordinates_Kronstad = (600, 750)
 coordinates_LilleLongGardsvann = (450, 335)
-days_interval = (1,365)
+days_interval = (1,366)
 marked_point = (0,0)
 
 
@@ -61,7 +68,7 @@ marked_point = (0,0)
 def on_day_interval(kvartal):
     global days_interval, marked_point
     axNok.cla()
-    days_interval = (1,365)
+    days_interval = (1,366)
     if kvartal == '1. Kvartal':
         days_interval = (1,90)
     if kvartal == '2. Kvartal':
@@ -69,7 +76,7 @@ def on_day_interval(kvartal):
     if kvartal == '3. Kvartal':
         days_interval = (180,270)
     if kvartal == '4. Kvartal':
-        days_interval = (270,365)
+        days_interval = (270,366)
     marked_point = (0, 0)
     plot_graph()
 
@@ -135,26 +142,28 @@ def plot_graph():
     kron_nox = kron_nox_year[days_interval[0]:days_interval[1]]
     days = len(nord_nox)
     lille_nox = [lille_longgardsvann(nord_nox[i], kron_nox[i]) for i in range(days)]
+    gjen_nox = gjen_nox_year[days_interval[0]:days_interval[1]]
 
 
     list_days = np.linspace(1, days, days)
 
 #draw the marked point & the orange graph
-    l4 = None
+    l5 = None
     if marked_point != (0,0):
         nox_point = [calc_point_value(nord_nox[i], kron_nox[i]) for i in range(days)]
-        l4, = axNok.plot(list_days, nox_point, 'darkorange')
+        l5, = axNok.plot(list_days, nox_point, 'darkorange')
         circle = mpatches.Circle((marked_point[0], marked_point[1]), 25, color='orange')
         axBergen.add_patch(circle)
 
     l1, = axNok.plot(list_days, nord_nox, 'blue')
     l2, = axNok.plot(list_days, kron_nox, 'red')
     l3, = axNok.plot(list_days, lille_nox, 'green')
+    l4, = axNok.plot(list_days,gjen_nox,'black' )
     axNok.set_title("NOX verdier")
     axInterval.set_title("Intervall")
 
-    lines = [l1, l2, l3] if l4 is None else [l1,l2, l3, l4]
-    axNok.legend(lines, ["Nordnes", "Kronstad", "Lille Longgårdsvann", "Markert plass"])
+    lines = [l1, l2, l3, l4] if l5 is None else [l1,l2, l3, l4, l5]
+    axNok.legend(lines, ["Nordnes", "Kronstad", "Lille Longgårdsvann","Gjennomsnitt", "Markert plass"])
     axNok.grid(linestyle='--')
     draw_label_and_ticks()
 
